1f33f6c5ddad7b9ffd603f51a95348219aa83e29
Md. Forhad Hossain <141973117+m-forhad@users.noreply.github.com>
2025-11-28 11:47:30 +0600

    change in camera movement

--- main.js
+++ main.js
@@ -291,7 +291,7 @@ const positions = [
       new THREE.Vector3(-150, 725, -258),
       new THREE.Vector3(-150, 410, -258),
       new THREE.Vector3(-150, 390, -258),
-      new THREE.Vector3(-50, 390, -258), //new
+      new THREE.Vector3(50, 390, -258), //new
       new THREE.Vector3(110, 300, -540),
       new THREE.Vector3(140, 300, -540),
       new THREE.Vector3(580, 500, 680),
@@ -432,4 +432,5 @@ cardsContainer.onclick = function() {
   document.getElementById("cards").innerHTML = "";
 
 }
+
 
